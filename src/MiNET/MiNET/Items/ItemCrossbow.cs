dfe93393e907d20b40c3159559b106ba740c0408
Antoine Langevin <1909451@crosemont.qc.ca>
2024-11-13 10:57:35 -0500

    Implemented crossbow
    Known issue :
    - For the first reload, if you don't release the item, it will show in the client as unloaded even tho it is
    - After the first shoot, it'll automatically shoot after reloading if you don't release

--- src/MiNET/MiNET/Items/ItemCrossbow.cs
+++ src/MiNET/MiNET/Items/ItemCrossbow.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using fNbt;
+using MiNET.Entities.Projectiles;
+using MiNET.Utils;
 using MiNET.Utils.Vectors;
 using MiNET.Worlds;
 
@@ -6,17 +9,122 @@ namespace MiNET.Items
 {
 	public class ItemCrossbow : Item
 	{
+		private long _useTime = 0;
+		private bool _isLoading = false;
+		private bool _hasReleased = false;
+
 		public ItemCrossbow() : base("minecraft:crossbow", 471)
 		{
 			MaxStackSize = 1;
+			ItemType = ItemType.Bow;
+			ExtraData = new NbtCompound();
 		}
 
 		public override void UseItem(Level world, Player player, BlockCoordinates blockCoordinates)
 		{
-			base.UseItem(world, player, blockCoordinates);
-			Console.WriteLine("Called use item");
+			if (!_isLoading)
+			{
+				_useTime = Environment.TickCount64;
+				_isLoading = true;
+			}
+
+			if (ExtraData is NbtCompound extraData && extraData["chargedItem"] is NbtCompound chargedItem)
+			{
+				if(!_hasReleased) return;
+
+				string itemName = (chargedItem["Name"] as NbtString)?.Value;
+				short damage = (chargedItem["Damage"] as NbtShort)?.Value ?? 0;
+				byte count = (chargedItem["Count"] as NbtByte)?.Value ?? 0;
+
+				var itemInCrossbow = ItemFactory.GetItem(itemName);
+				itemInCrossbow.Count = count;
+				itemInCrossbow.Damage = damage;
+
+				ExtraData.Clear();
+				player.SendPlayerInventory();
+				float force = CalculateForce();
+				if(itemInCrossbow is ItemArrow)
+				{
+					var arrow = new Arrow(player, world, 2, !(force < 1.0));
+					arrow.PowerLevel = this.GetEnchantingLevel(EnchantingType.Power);
+					arrow.KnownPosition = (PlayerLocation) player.KnownPosition.Clone();
+					arrow.KnownPosition.Y += 1.62f;
+
+					arrow.Velocity = arrow.KnownPosition.GetHeadDirection().Normalize() * (force * 3.5f);
+					arrow.KnownPosition.Yaw = (float) arrow.Velocity.GetYaw();
+					arrow.KnownPosition.Pitch = (float) arrow.Velocity.GetPitch();
+					arrow.BroadcastMovement = true;
+					arrow.DespawnOnImpact = false;
+					arrow.SpawnEntity();
+				}
+				player.Inventory.DamageItemInHand(ItemDamageReason.ItemUse, player, null);
+
+				world.BroadcastSound(player.KnownPosition, LevelSoundEventType.CrossbowShoot);
+				
+				_isLoading = false;
+			}
+			else
+			{
+				long timeUsed = Environment.TickCount64 - _useTime;
+				if (timeUsed >= 1100)
+				{
+					world.BroadcastSound(player.KnownPosition, LevelSoundEventType.CrossbowLoadingEnd);
+					ExtraData = new NbtCompound()
+					{
+						new NbtCompound("chargedItem")
+						{
+							new NbtString("Name", "minecraft:arrow"),
+							new NbtShort("Damage", 0),
+							new NbtByte("Count", 1)
+						}
+					};
+					player.SendPlayerInventory();
+					_isLoading = false;
+				} else
+				{
+					world.BroadcastSound(player.KnownPosition, LevelSoundEventType.CrossbowLoadingStart);
+				}
+			}
 		}
 
+		public override void Release(Level world, Player player, BlockCoordinates blockCoordinates)
+		{
+			long timeUsed = Environment.TickCount64 - _useTime;
+			if (timeUsed >= 1100)
+			{
+				world.BroadcastSound(player.KnownPosition, LevelSoundEventType.CrossbowLoadingEnd);
+				ExtraData = new NbtCompound()
+				{
+					new NbtCompound("chargedItem")
+					{
+						new NbtString("Name", "minecraft:arrow"),
+						new NbtShort("Damage", 0),
+						new NbtByte("Count", 1)
+					}
+				};
+				player.SendPlayerInventory();
+			}
+			
+			_isLoading = false;
+			_hasReleased = true;
+		}
 
+		private float CalculateForce()
+		{
+			float force = 1250;
+			force = ((force * force) + (force * 2.0f)) / 3.0f;
+
+			if (force < 0.5f)
+			{
+				return 0;
+			}
+
+			if (force > 1.0f)
+			{
+				force = 1.0f;
+			}
+
+			return force;
+		}
 	}
 }
