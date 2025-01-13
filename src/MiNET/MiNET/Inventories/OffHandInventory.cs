ec922c603f7266a937ce0c53d3cd86f478594fec
Antoine Langevin <1909451@crosemont.qc.ca>
2025-01-12 19:56:21 -0500

    ...

--- src/MiNET/MiNET/Inventories/OffHandInventory.cs
+++ src/MiNET/MiNET/Inventories/OffHandInventory.cs
@@ -1,29 +1,21 @@
-﻿using log4net;
-using MiNET.Items;
+﻿using MiNET.Items;
 using MiNET.Net;
 
 namespace MiNET.Inventories;
 
-public class OffHandInventory
+public class OffHandInventory(Player player)
 {
-	private static readonly ILog Log = LogManager.GetLogger(typeof(OffHandInventory));
-
 	public static readonly byte WindowId = 119;
 	public static readonly byte InventoryId = 34;
 
-	private Item Item = new ItemAir();
-	public Player Holder { get; set; }
+	private Item _item = new ItemAir();
+	public Player Holder { get; set; } = player;
 
-	public OffHandInventory(Player player)
-	{
-		Holder = player;
-	}
-
-	public Item GetItem() => Item;
+	public Item GetItem() => _item;
 
 	public void SetItem(Item item, bool sendUpdate = true)
 	{
-		Item = item;
+		_item = item;
 		if (sendUpdate)
 			SendUpdate();
 	}
@@ -35,13 +27,13 @@ public class OffHandInventory
 		sendMobEquipment.selectedSlot = 0;
 		sendMobEquipment.slot = 0;
 		sendMobEquipment.windowsId = WindowId;
-		sendMobEquipment.item = Item;
+		sendMobEquipment.item = _item;
 		Holder.Level.RelayBroadcast(sendMobEquipment);
 
 		var sendSlotUpdate = McpeInventorySlot.CreateObject();
 		sendSlotUpdate.inventoryId = InventoryId;
 		sendSlotUpdate.slot = 0;
-		sendSlotUpdate.storageItem = Item;
+		sendSlotUpdate.storageItem = _item;
 		Holder.SendPacket(sendSlotUpdate);
 	}
 }
